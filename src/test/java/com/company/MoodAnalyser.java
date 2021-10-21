6d746f8868dfc03f782927e6cffa857169854b77
santoshlearner07 <nandiyawarsantosh.0719@gmail.com>
2021-10-21 21:07:31 +0530

    Null Pointer Exception Added to Test Case

--- src/test/java/com/company/MoodAnalyser.java
+++ src/test/java/com/company/MoodAnalyser.java
@@ -1,10 +1,26 @@
 package com.company;
 
 public class MoodAnalyser {
-    public String analysemood(String message) {
-        if (message.contains("SAD"))
+
+    public static String analyseMood() {
+        if (" ".contains("SAD"))
             return "SAD";
         else
             return "HAPPY";
     }
+
+    public static String analyseMood(String message) {
+        try {
+            if (message.contains("SAD"))
+                return "SAD";
+            else
+                return "HAPPY";
+        } catch (NullPointerException e) {
+            return "HAPPY";
+        }
+    }
+//    public static void main(String[] args) {
+//        analyseMood();
+//        System.out.println(analyseMood());
+//    }
 }
--- src/test/java/com/company/MoodAnalyserTest.java
+++ src/test/java/com/company/MoodAnalyserTest.java
@@ -7,14 +7,35 @@ public class MoodAnalyserTest {
     @Test
     public void givenMessage_WhenSad_ReturnSad() {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analysemood("I am in SAD mood");
+        String mood = MoodAnalyser.analyseMood("I am in SAD mood");
         Assert.assertEquals("SAD", mood);
     }
 
     @Test
-    public void givenMessage_WhenNotSad_ReturnHappy(){
+    public void givenMessage_WhenNotSad_ReturnHappy() {
         MoodAnalyser moodAnalyser = new MoodAnalyser();
-        String mood = moodAnalyser.analysemood("This is a happy message");
-        Assert.assertEquals("HAPPY",mood);
+        String mood = moodAnalyser.analyseMood("This is a HAPPY message");
+        Assert.assertEquals("HAPPY", mood);
+    }
+
+    @Test
+    public void givenNullMoodReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood(null);
+        Assert.assertEquals("HAPPY", mood);
+    }
+
+    @Test
+    public void whenCalled_WithoutParam_ReturnSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = MoodAnalyser.analyseMood();
+        Assert.assertEquals("SAD", mood);
+    }
+
+    @Test
+    public void whenCalled_WithoutParam_ReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = MoodAnalyser.analyseMood();
+        Assert.assertEquals("HAPPY", mood);
     }
 }
